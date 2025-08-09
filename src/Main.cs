440cabc415f40f42061cd5e7dbfdeee866652e25
BarackOBusiness <nichjoh21@gmail.com>
2025-08-09 12:08:37 -0400

    Update integration for first support of custom maps

--- src/Main.cs
+++ src/Main.cs
@@ -5,16 +5,30 @@ using UnityEngine.SceneManagement;
 
 namespace LivesplitIntegration;
 
-[BepInPlugin("goi.core.livesplitintegration", "Livesplit Integration", "0.1.0")]
+// Remember to synchronize this reference to the autosplitter when editing the state mask
+// Reference for the game state bitmask:
+// 0001    
+//    └─── "Is paused?" bit
+// 0010
+//   └──── "Should reset?" bit
+// 1100
+// └┼───── Scene bits:
+//  ├───── 00: Main Menu
+//  ├───── 01: Default Map (Mian Scene)
+//  ├───── 10: Reward
+//  └───── 11: Custom Map
+// All following these bits is a mapping to custom levels. << 4 all values to get their appropriate bit positions.
+// 1: Cavern Map ("Gems and Minerals")
+
+[BepInPlugin("goi.core.livesplitintegration", "Livesplit Integration", "0.2.0")]
 public class LivesplitIntegration : BaseUnityPlugin
 {
     public static float xPos = 0;
     public static float yPos = 0;
     public static float time = 0;
-    public static int state = 2; // Start on main menu
+    public static int state = 0b0000; // Start on main menu, reset and paused bits off.
 
     private Rigidbody2D player;
-    private bool isResetting;
 
     private void Awake()
     {
@@ -23,15 +37,11 @@ public class LivesplitIntegration : BaseUnityPlugin
         SceneManager.sceneLoaded += OnSceneLoad;
     }
 
-    private void LateUpdate() {
-        if ((state & 0b1000) == 8 && !isResetting) {
-            StartCoroutine(UnsetResetBit());
-        }
-
+    private void Update() {
         if (!player)
             return;
 
-        state = Time.timeScale == 0f? state | 0b100 : state & ~0b100;
+        state = Time.timeScale == 0f? state | 0b1 : state & ~0b1;
 
         xPos = player.position.x;
         yPos = player.position.y;
@@ -39,34 +49,42 @@ public class LivesplitIntegration : BaseUnityPlugin
     }
 
     private IEnumerator UnsetResetBit() {
-        isResetting = true;
         while (true) {
             yield return new WaitForFixedUpdate();
-            break; // Just yield one fixed update...
+            break; // Just yield one fixed update
         }
-        state = state & ~0b1000;
-        isResetting = false;
+        state = state & ~0b10;
     }
 
+    // On any scene load that demands the timer be reset, load the coroutine to
+    // unset the reset bit one tick later.
     private void OnSceneLoad(Scene scene, LoadSceneMode mode) {
+        // Motivations for doing this are to determine whether multiple scenes will load when loading a custom map
+        Logger.LogInfo($"Scene loaded: {scene.name}");
         switch (scene.name) {
+            case "Loader":
+                state = (state & 0) | 0b0010; // Set scene to man menu and set reset bit on
+                break;
             case "Mian":
+                state = (state & 0) | 0b0110; // Set scene to man and set reset bit on
                 player = GameObject.Find("Player").GetComponent<Rigidbody2D>();
-                state = (state & ~0b11) | 0b1000; // Set scene to man and set reset bit on
                 time = 0f; xPos = 0f; yPos = 0f;
                 break;
             case "Reward Loader":
-                state = (state & ~0b11) | 0b01; // Set scene to end screen
-                break;
             case "Reward Loader Offline":
-                state = (state & ~0b11) | 0b01;
+                state = (state & 0) | 0b1000; // Set scene to end screen
                 break;
-            case "Loader":
-                state = (state & ~0b11) | 0b1010; // Set scene to man menu and set reset bit on
+            case "Gems and Minerals":
+                state = (state & 0) | 0b11100; // Set scene to custom and map ID to cavern
                 break;
             default:
-                state = state | ~0b1011; // Set scene to unknown and reset bit on so no timing occurs
+                state = (state & 0) | 0b1110; // Set scene to custom and reset bit on so no timing occurs
                 break;
+        }
+
+        if ((state & 0b10) == 2) {
+            // If we're resetting, yield one tick for the autosplitter then unset the reset bit
+            StartCoroutine(UnsetResetBit());
         }
     }
 }
